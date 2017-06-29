af7a2d23ab3f281e5d5e1648e27564e64d5c8c2f
Nick Ford <nickforddesign@gmail.com>
2017-06-29 16:32:02 -0400

    updated hook

--- build/version.js
+++ build/version.js
@@ -1,6 +1,7 @@
 'use strict'
 const patch = require('cordova-auto-patch');
+const root_dir = require('app-root-dir').get();
 const version = require('../package.json').version
 
 console.log(version);
-patch('../', version);
+patch(root_dir, version);
