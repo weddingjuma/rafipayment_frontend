3784980729de027d2aaee6c94d61075732ce9473
Nick Ford <nickforddesign@gmail.com>
2017-06-29 17:39:54 -0400

    cleaning up versioning

--- build/version.js
+++ build/version.js
@@ -2,6 +2,4 @@
 const patch = require('cordova-auto-patch');
 const root_dir = require('app-root-dir').get();
 const version = require('../package.json').version
-
-console.log(version);
 patch(`${root_dir}/`, version);
