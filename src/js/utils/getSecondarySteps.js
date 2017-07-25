dd7d6b0c6642feebbe6e96cb19b5ffefd5d871ab
Nick Ford <nickforddesign@gmail.com>
2017-07-25 17:49:11 -0400

    onboarding 100% coverage, working on request util coverage

--- src/js/utils/getSecondarySteps.js
+++ src/js/utils/getSecondarySteps.js
@@ -5,29 +5,29 @@ export default (user) => {
 
   const is_tenant = ['tenant'].includes(user.role)
 
-  if (!is_tenant) return
+  if (is_tenant) {
+    user.leases.forEach(function(lease) {
+      const split = _.get(lease.split, user.id)
 
-  user.leases.forEach(function(lease) {
-    const split = _.get(lease.split, user.id)
+      if (split === undefined) {
+        // console.log('split missing', lease);
+        steps.push({
+          name: 'show-lease',
+          value: split,
+          lease: lease
+        })
 
-    if (split === undefined) {
-      console.log('split missing', lease);
-      steps.push({
-        name: 'show-lease',
-        value: split,
-        lease: lease
-      })
+        steps.push({
+          name: 'split',
+          value: split,
+          lease: lease
+        })
+      }
+    })
 
-      steps.push({
-        name: 'split',
-        value: split,
-        lease: lease
-      })
-    }
-  })
-
-  return steps.map(function(step, index) {
-    step.index = index
-    return step
-  })
+    return steps.map(function(step, index) {
+      step.index = index
+      return step
+    })
+  }
 }
---
+++ src/js/utils/tests/onboarding.spec.js
@@ -0,0 +1,71 @@
+import User from '@/models/user'
+
+describe('$user.onboarding_steps - no lease', () => {
+  const user = new User({
+    role: 'tenant'
+  })
+  it('should push no-lease for a user with no lease', () => {
+    expect(user.onboarding_steps[0].name)
+      .toBe('no-lease')
+  })
+})
+
+describe('$user.onboarding_steps - tenants', () => {
+  const user = new User({
+    role: 'tenant',
+    leases: [
+      1
+    ]
+  })
+  it('should push accept terms for new tenant', () => {
+    expect(user.onboarding_steps[0].name)
+      .toBe('terms')
+  })
+
+  it('should push dwolla-bank for new tenant', () => {
+    expect(user.onboarding_steps[1].name)
+      .toBe('dwolla-bank')
+  })
+
+  it('should push show-lease for new tenant', () => {
+    expect(user.onboarding_steps[2].name)
+      .toBe('show-lease')
+  })
+
+  it('should push split for new tenant', () => {
+    expect(user.onboarding_steps[3].name)
+      .toBe('split')
+  })
+
+  it('should push password for new tenant', () => {
+    expect(user.onboarding_steps[4].name)
+      .toBe('set-password')
+  })
+})
+
+describe('$user.onboarding_steps - admins', () => {
+  const user = new User({
+    role: 'admin',
+    company: {}
+  })
+  it('should push dwolla-account for first admin', () => {
+    expect(user.onboarding_steps[0].name)
+      .toBe('dwolla-account')
+  })
+})
+
+describe('$user.secondary_steps - show-lease', () => {
+  const user = new User({
+    role: 'tenant',
+    leases: [{}]
+  })
+  it('should push show lease for a user with a lease missing split', () => {
+    expect(user.secondary_steps[0].name)
+      .toBe('show-lease')
+  })
+
+  it('should push split for a user with a lease missing split', () => {
+    expect(user.secondary_steps[1].name)
+      .toBe('split')
+  })
+})
